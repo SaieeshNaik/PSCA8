f360283d689ab0bd4a3007cc5f88e0d00393bdc1
Dell <Dell@DESKTOP-8GLS92U>
2022-03-02 14:09:44 +0530

    1st commit

---
+++ src/Pcsa8.java
@@ -0,0 +1,11 @@
+
+public class Pcsa8
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello from local");
+		System.out.println("my token...........> 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");
+		
+	}
+
+}
